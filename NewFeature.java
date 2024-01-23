be9f88d28eb53a3ff8a53e050a9ecce7f4f013c3
mouhib <iwritebadcode@github.com>
2024-01-23 23:08:38 +0100

    edit message

--- NewFeature.java
+++ NewFeature.java
@@ -1,6 +1,6 @@
 public class NewFeature {
     public static void main(String[] args) {
-        System.out.println("Hello, from new feature!");
+        System.out.println("Hello, from 4.28");
     }
 }
 
