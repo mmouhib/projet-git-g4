926cd377b943147a595124b8a9b350d9511ba73d
Yosri Jaziri <yjaziri.yestunisia2016@gmail.com>
2024-01-23 23:25:55 +0100

    Yosri's changes to new feature

--- NewFeature.java
+++ NewFeature.java
@@ -1,6 +1,7 @@
 public class NewFeature {
     public static void main(String[] args) {
-        System.out.println("Hello, from 4.28");
+        System.out.println("Can the real slim shady please stand up?");
+        System.out.println("Hello, from 4.28. This is Yosri speaking!");
     }
 }
 
