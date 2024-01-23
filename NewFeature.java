dd995b7ebed67e22715c7d936e48ea8ea65d8a7a
Salma Maamouri <salma.maamouri@axefinance.com>
2024-01-24 00:25:09 +0100

    Commit Arbre

--- NewFeature.java
+++ NewFeature.java
@@ -2,5 +2,10 @@ public class NewFeature {
     public static void main(String[] args) {
         System.out.println("Hello, from 4.28");
     }
+    public static void main(String[] args) {
+        System.out.println("Hello, salma");
+    }
+
+
 }
 
