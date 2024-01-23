948c775413968d914cc72746df32b31730b03438
YHbibi <hbibiyoussef27@gmail.com>
2024-01-23 22:00:36 +0100

    make an explicit error in line 118

--- src/simplejavacalculator/Calculator.java
+++ src/simplejavacalculator/Calculator.java
@@ -115,7 +115,7 @@ public class Calculator {
             return log10(num);
         }
         if (newMode == MonoOperatorModes.ln) {
-            return log(num);
+            return ln(num);
         }
         if (newMode == MonoOperatorModes.rate) {
            return num / 100;
