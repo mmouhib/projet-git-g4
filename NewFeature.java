4d24ae111e1f9b533bc8b726af55dfb8fc9288b4
YHbibi <hbibiyoussef27@gmail.com>
2024-01-23 23:51:30 +0100

    version 4.30

--- NewFeature.java
+++ NewFeature.java
@@ -1,6 +1,6 @@
 public class NewFeature {
     public static void main(String[] args) {
-        String version = "4.28";
+        String version = "4.30";
         System.out.println( version + " is released");
         
     }
